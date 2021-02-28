a49609ed4bb1be68069d7c8b5ebdde32fe4ecd8d
Szymon Cader <szymon.sc.cader@gmail.com>
2021-02-28 15:09:27 +0100

    Setup batch notifications

--- shargain/notifications/models.py
+++ shargain/notifications/models.py
@@ -1,20 +1,26 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
-class NotificationPlatformChoices(models.TextChoices):
+class NotificationChannelChoices(models.TextChoices):
     DISCORD = "discord", "Discord"
     TELEGRAM = "telegram", "Telegram"
 
 
-class NotificationChannel(models.Model):
-    name = models.CharField(max_length=100)
-    platform = models.CharField(
-        max_length=200, choices=NotificationPlatformChoices.choices
+class NotificationConfig(models.Model):
+    name = models.CharField(verbose_name=_("Name"), max_length=100)
+    channel = models.CharField(
+        verbose_name=_("Channel"),
+        max_length=200,
+        choices=NotificationChannelChoices.choices,
     )
-    webhook_url = models.URLField(blank=True)
-    token = models.CharField(max_length=100, blank=True)
+    webhook_url = models.URLField(verbose_name=_("Webhook url"), blank=True)
+    token = models.CharField(verbose_name=_("Token"), max_length=100, blank=True)
+    chatid = models.CharField(verbose_name=_("Chat ID"), max_length=100, blank=True)
 
     class Meta:
         verbose_name = _("Notification channel")
-        verbose_name_plural = ""
+        verbose_name_plural = _("Notification channels")
+
+    def __str__(self):
+        return self.name
---
+++ shargain/notifications/services.py
@@ -0,0 +1,54 @@
+from typing import List
+
+from shargain.notifications.models import NotificationChannelChoices
+from shargain.notifications.senders import TelegramNotificationSender
+from shargain.offers.models import Offer, ScrappingTarget
+
+
+class NewOfferNotificationService:
+    def __init__(self, offers: List[Offer], scrapping_target: ScrappingTarget):
+        self.offers = offers
+        self._scrapping_target = scrapping_target
+
+    def run(self):
+        message = self.get_message_header()
+        for offer in self.offers:
+            offer_message = self.get_message_for_offer(offer)
+            if len(message + offer_message) > self.get_maximum_message_length(
+                self._scrapping_target.notification_config.channel
+            ):
+                self._send(message)
+                message = self.get_message_header() + offer_message
+            else:
+                message += offer_message
+        self._send(message)
+
+    def _send(self, message):
+        notification_sender = self._get_notification_sender_class()(
+            self._scrapping_target.notification_config
+        )
+        notification_sender.send(message)
+
+    def _get_notification_sender_class(self):
+        return self.get_notification_sender_class(
+            self._scrapping_target.notification_config.channel
+        )
+
+    @staticmethod
+    def get_maximum_message_length(notification_channel):
+        return {NotificationChannelChoices.TELEGRAM: 4096}[notification_channel]
+
+    @staticmethod
+    def get_notification_sender_class(notification_channel):
+        return {NotificationChannelChoices.TELEGRAM: TelegramNotificationSender}[
+            notification_channel
+        ]
+
+    def get_message_for_offer(self, offer):
+        return (
+            f"{offer.title} ({offer.published_at.time()}) "
+            f"za {offer.price}zł\n{offer.url}\n\n"
+        )
+
+    def get_message_header(self):
+        return f"{self._scrapping_target.name.upper()}\n\n"
