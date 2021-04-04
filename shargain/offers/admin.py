c7ffc08f85a3097af891420c83bec54d3a314555
Szymon Cader <szymon.sc.cader@gmail.com>
2021-04-04 11:20:10 +0200

    Tweak offers admin

--- shargain/offers/admin.py
+++ shargain/offers/admin.py
@@ -1,3 +1,6 @@
+import textwrap
+from datetime import timedelta
+
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy
@@ -9,9 +12,22 @@ from shargain.offers.models import Offer, ScrappingTarget
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
-    list_display = ("title", "price", "get_is_open", "get_link", "published_at")
-    list_filter = ["target", ]
+    list_display = (
+        "get_title",
+        "price",
+        "get_link",
+        "published_at",
+        "get_closed_at",
+        "get_duration",
+    )
+    list_filter = [
+        "target",
+    ]
     ordering = ("-published_at",)
+
+    @admin_display(short_description=gettext_lazy("Title"), admin_order_field="title")
+    def get_title(self, obj):
+        return mark_safe("<br>".join(textwrap.wrap(obj.title, width=30)))
 
     @admin_display(short_description=gettext_lazy("Link to offer"))
     def get_link(self, obj):
@@ -19,12 +35,25 @@ class OfferAdmin(admin.ModelAdmin):
         return mark_safe(f"<a href='{obj.url}'>{button_text}</a>")
 
     @admin_display(
-        short_description=gettext_lazy("Opened"),
-        boolean=True,
+        short_description=gettext_lazy("Closed at"),
         admin_order_field="closed_at",
     )
-    def get_is_open(self, obj):
-        return not bool(obj.closed_at)
+    def get_closed_at(self, obj):
+        css_class = "btn-outline-danger" if obj.closed_at else "btn-outline-success"
+        if obj.closed_at:
+            return mark_safe(
+                f"<div class='{css_class}'>"
+                f"{obj.closed_at.strftime('%d-%m-%y %H:%M')}"
+                f"</div>"
+            )
+
+    @admin_display(
+        short_description=gettext_lazy("Duration"),
+    )
+    def get_duration(self, obj):
+        if obj.closed_at:
+            seconds = round((obj.closed_at - obj.created_at).total_seconds())
+            return timedelta(seconds=seconds)
 
 
 @admin.register(ScrappingTarget)
