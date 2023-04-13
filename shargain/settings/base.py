7d63a61d74b5a9d437e0391bec9f4242be3fffb9
Szymon Cader <szymon.sc.cader@gmail.com>
2023-04-13 19:26:16 +0200

    Cleanup settings

--- shargain/settings/base.py
+++ shargain/settings/base.py
@@ -48,8 +48,6 @@ LOCAL_APPS = [
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 # ------------- MIDDLEWARES -------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -58,7 +56,7 @@ MIDDLEWARE = [
     "django.middleware.locale.LocaleMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
