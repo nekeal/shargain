b660dd7874840f34f4f1df2a21d850fd62f613de
Szymon Cader <szymon.sc.cader@gmail.com>
2025-08-28 08:51:40 +0200

    feat(frontend): Replace Header component with Navbar01 for improved navigation

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,8 @@
 import { Outlet, createRootRouteWithContext, useMatchRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import Header from '../components/Header'
 import { AuthGuard } from '../components/auth/auth-guard'
 import type { QueryClient } from '@tanstack/react-query'
+import { Navbar01 } from "@/components/ui/shadcn-io/navbar-01";
 
 interface RouterContext {
   queryClient: QueryClient
@@ -22,7 +22,27 @@ function RootComponent() {
       ) : (
         <AuthGuard>
           <>
-            <Header />
+            <Navbar01
+              logo={<div className="bg-violet-600 text-white rounded-lg w-10 h-10 flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                </svg>
+              </div>}
+              navigationLinks={
+                [
+                  {
+                    label: 'Dashboard',
+                    href: '/dashboard',
+                    active: !!matchRoute({ to: '/dashboard' })
+                  },
+                  {
+                    label: 'Notifications',
+                    href: '/notifications',
+                    active: !!matchRoute({ to: '/notifications' })
+                  },
+                ]
+              } />
+            {/* <Header /> */}
             <Outlet />
             <TanStackRouterDevtools />
           </>
